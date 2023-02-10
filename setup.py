45668232398af12cd1b0eab5dee60aee49f09954
Caroline Robinson <CRobinsonAtWork@gmail.com>
2023-02-10 07:13:15 -0900

    latest version asks for address twice

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.util import convert_path
 
 standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
 standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
 def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
     out = {}
     stack = [(convert_path(where), '', package)]
@@ -53,7 +54,7 @@ setup(name='docassemble.ChildNameChangePetitionCiv694',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.15.0'],
+      install_requires=['docassemble.AssemblyLine>=2.19.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ChildNameChangePetitionCiv694/', package='docassemble.ChildNameChangePetitionCiv694'),
      )
